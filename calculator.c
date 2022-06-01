7a7ace54d2b1af1adbdacee34caa9becb2c09927
daksh <daksh0400.be21@chitkara.edu.in>
2022-06-01 23:40:43 +0530

    1

--- calculator.c
+++ calculator.c
@@ -31,10 +31,14 @@ int main() {
       break;
 
     default:
-      // If the operator is other than +, -, * or /, error message is shown
-      cout << "Error! operator is not correct";
+    	
+      cout << "Error! operator is not correct";\\
       break;
   }
 
   return 0;
 }
+
+
+
+
